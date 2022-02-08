0909eb83c7e1d215ae5ce1b26e69bd62046754aa
Josh <barlow.joshua@gmail.com>
2022-02-08 23:24:33 +0000

    added auto-mask

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # animation-painter
 A genetic drawing experiment trying to make an animated painting from an input image sequence.
 
-<img src="/examples/test_01.jpg?raw=true" width="600">
+<img src="/examples/test_02.jpg?raw=true" width="600">
 
 Built with python 3.8.7 and the following libraries:
 - numpy 1.22.1
--- animPaint.py
+++ animPaint.py
@@ -60,7 +60,7 @@ brush = brushImage.astype(float) / 255
 brushSmall = brushSmallImage.astype(float) / 255
 brushSmaller = brushSmallerImage.astype(float) / 255
 
-for x in range(1500):
+for x in range(400):
     randX = int(random.random() * imageWidth)
     randY = int(random.random() * imageHeight)
 
@@ -113,13 +113,141 @@ for x in range(1500):
 
 comparisonImage =  np.absolute(cv2.subtract(inputImage, baseImage, dtype=cv2.CV_64F).astype(int))
 
-print(comparisonImage.dtype)
+print('stage 1')
+# print(comparisonImage[:,:])
 
 comparisonImage = cv2.cvtColor(np.uint8(comparisonImage), cv2.COLOR_BGR2GRAY)
+comparisonImage = cv2.blur(comparisonImage, (30,30))
+
+for x in range(800):
+
+    maskValue = 0
+    while(maskValue < 20):
+        randX = int(random.random() * imageWidth)
+        randY = int(random.random() * imageHeight)
+
+        maskValue = comparisonImage[randY,randX]
+        # print(maskValue)
+    # print('Using: ' + str(maskValue))
+
+    # print('x: ' + str(randX) + ' y: ' + str(randY))
+
+    # print(inputImage[randY][randX])
+
+    inBrush = brushSmall
+
+    if(x>300):
+        inBrush = brushSmaller
+
+    rotatedBrush = cv2.warpAffine(inBrush,generateRandomRotation(300, 300),(brush.shape[1],brush.shape[0]))
+
+    border = cv2.copyMakeBorder(
+            rotatedBrush,
+            top=0,
+            bottom=brushTopBorder,
+            left=0,
+            right=brushRightBorder,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[0]
+                )
+        
+    sampleBrushImage = cv2.warpAffine(border,generateTransform(randX, randY),(imageWidth,imageHeight))
+
+    sampleBrushImage = np.tile(sampleBrushImage[:, :, None], [1, 1, 3])
+
+    targetColor = blankImage.copy()
+    targetColor[:,:] = inputImage[randY][randX].astype(int)
+    targetColor = cv2.convertScaleAbs(targetColor)
+
+    # print(targetColor.shape)
+    # print(targetColor[randY][randX])
+
+    # print(blankImage.dtype)
+    # print(sampleBrushImage.dtype)
+    # print(targetColor.dtype)
+
+    sampleImage = cv2.add( cv2.multiply(baseImage.astype(float), cv2.subtract(blankImage.astype(float), sampleBrushImage.astype(float)) ), cv2.multiply(targetColor.astype(float), sampleBrushImage.astype(float)) )
+
+    difference = calcDifference(inputImage, sampleImage)
+    diffBase = calcDifference(inputImage, baseImage)
+
+    if (difference <= diffBase):
+        baseImage = cv2.convertScaleAbs(sampleImage)
+
+print('stage 2')
+
+comparisonImage =  np.absolute(cv2.subtract(inputImage, baseImage, dtype=cv2.CV_64F).astype(int))
+
+
+comparisonImage = cv2.cvtColor(np.uint8(comparisonImage), cv2.COLOR_BGR2GRAY)
+comparisonImage = cv2.blur(comparisonImage, (10,10))
+
+for x in range(500):
+
+    maskValue = 0
+    while(maskValue < 50):
+        randX = int(random.random() * imageWidth)
+        randY = int(random.random() * imageHeight)
+
+        maskValue = comparisonImage[randY,randX]
+        # print(maskValue)
+    # print('Using: ' + str(maskValue))
+
+    # print('x: ' + str(randX) + ' y: ' + str(randY))
+
+    # print(inputImage[randY][randX])
+
+    inBrush = brushSmall
+
+    if(x>1):
+        inBrush = brushSmaller
+
+    rotatedBrush = cv2.warpAffine(inBrush,generateRandomRotation(300, 300),(brush.shape[1],brush.shape[0]))
+
+    border = cv2.copyMakeBorder(
+            rotatedBrush,
+            top=0,
+            bottom=brushTopBorder,
+            left=0,
+            right=brushRightBorder,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[0]
+                )
+        
+    sampleBrushImage = cv2.warpAffine(border,generateTransform(randX, randY),(imageWidth,imageHeight))
+
+    sampleBrushImage = np.tile(sampleBrushImage[:, :, None], [1, 1, 3])
+
+    targetColor = blankImage.copy()
+    targetColor[:,:] = inputImage[randY][randX].astype(int)
+    targetColor = cv2.convertScaleAbs(targetColor)
+
+    # print(targetColor.shape)
+    # print(targetColor[randY][randX])
+
+    # print(blankImage.dtype)
+    # print(sampleBrushImage.dtype)
+    # print(targetColor.dtype)
+
+    sampleImage = cv2.add( cv2.multiply(baseImage.astype(float), cv2.subtract(blankImage.astype(float), sampleBrushImage.astype(float)) ), cv2.multiply(targetColor.astype(float), sampleBrushImage.astype(float)) )
+
+    difference = calcDifference(inputImage, sampleImage)
+    diffBase = calcDifference(inputImage, baseImage)
+
+    if (difference <= diffBase):
+        baseImage = cv2.convertScaleAbs(sampleImage)
+
+print('stage 3')
+
+for j in range(imageWidth):
+    for i in range(imageHeight):
+        # print(comparisonImage[i,j])
+        if(comparisonImage[i,j] > 20):
+            comparisonImage[i,j] = 255
 
 comparisonImage = np.tile(comparisonImage[:, :, None], [1, 1, 3])
 
-baseImage = np.concatenate((inputImage,baseImage), axis=1)
+baseImage = np.concatenate((inputImage,baseImage,comparisonImage), axis=1)
 
 cv2.imshow('image', baseImage)
 cv2.waitKey(0)
